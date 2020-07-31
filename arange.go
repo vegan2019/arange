388816e6a6e736a2c6e92d0edbb5eb033c3cee4d
vegan2019 <58324192+vegan2019@users.noreply.github.com>
2020-07-31 16:17:00 +0800

    Update arange.go

--- arange.go
+++ arange.go
@@ -13,7 +13,7 @@ should return
 [0 0.5 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 6 6.5 7 7.5 8 8.5 9 9.5]
 */
 
-func arange(start, stop, step float64) []float64 {
+func Arange(start, stop, step float64) []float64 {
 	N := int(math.Ceil((stop - start) / step))
 	rnge := make([]float64, N, N)
 	i := 0
