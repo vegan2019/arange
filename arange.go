7f193d9fa219cc28e786161ad891ac4b8d7e5a97
vegan2019 <58324192+vegan2019@users.noreply.github.com>
2020-07-31 16:08:21 +0800

    Create arange.go

---
+++ arange.go
@@ -0,0 +1,25 @@
+/*
+https://stackoverflow.com/a/19908214
+*/
+package arange
+
+import "math"
+
+/*
+for example:
+
+arange(0., 10., 0.5)
+should return
+[0 0.5 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 6 6.5 7 7.5 8 8.5 9 9.5]
+*/
+
+func arange(start, stop, step float64) []float64 {
+	N := int(math.Ceil((stop - start) / step))
+	rnge := make([]float64, N, N)
+	i := 0
+	for x := start; x < stop; x += step {
+		rnge[i] = x
+		i += 1
+	}
+	return rnge
+}
